487fd9d26221417d94f0b6aa71c9e98dfec33ba5
BrunoMiguelTeixeira <119333540+BrunoMiguelTeixeira@users.noreply.github.com>
2023-05-09 18:33:55 +0100

    Add files via upload
    Added problems to fix later.

---
+++ main.c
@@ -0,0 +1,251 @@
+/*
+ * Paulo Pedreiras, 2023/03
+ * 	Simple Digital I/O, using additional/external buttons
+ * 
+ * It shows the use of generic gpio functions. In these cases the pin functionality set by 
+ * 		the DT (if any) is not used.
+ *  
+ * Base documentation:
+ *        
+ *      HW info:
+ *          https://infocenter.nordicsemi.com/topic/struct_nrf52/struct/nrf52840.html
+ *          Section: nRF52840 Product Specification -> Peripherals -> GPIO / GPIOTE
+ * 
+ *          Board specific HW info can be found in the nRF52840_DK_User_Guide_20201203. I/O pins available at pg 27
+ *
+ *          Note that Leds are active-low. I.e., they turn on when a logic zero is output at the corresponding port, and vice-versa.
+ *          This behaviour can be reverted by adding the "GPIO_ACTIVE_LOW" flag - gpio_pin_config( ...., GPIO_OUTPUT_ACTIVE | GPIO_ACTIVE_LOW)
+ * 			These flags can be obtained from the gpio device structure (e.g. led1.dt_flags) and directly used on the pin_config function
+ *
+ *
+ *      SW API info:
+ *          https://developer.nordicsemi.com/nRF_Connect_SDK/doc/latest/zephyr/reference/peripherals/gpio.html 
+ * 
+ * 
+ * 	Notes:
+ * 		Note that the GPIO and PWM must be enabled in prj.conf 
+ * 		CONFIG_GPIO=y
+ * 		 
+ * 		Check prj.conf for additional contents that allows to see log messages 
+ * 		and also printk() output
+ * 
+ */
+
+/* Includes. Add according to the resources used  */
+#include <zephyr/kernel.h>          /* for k_msleep() */
+#include <zephyr/device.h>          /* for device_is_ready() and device structure */
+#include <zephyr/devicetree.h>		/* for DT_NODELABEL() */
+#include <zephyr/drivers/gpio.h>    /* for GPIO api*/
+#include <zephyr/sys/printk.h>      /* for printk()*/
+
+/* Use a "big" sleep time to reduce CPU load (button detection int activated, not polled) */
+#define SLEEP_TIME_MS   30				/*60 hz*/ 
+
+/* Set the pins used for LED and buttons */
+/* LED 1 and buttons 1-4 are the ones on board */
+/* buttons 5-8 are connected to pins labeled A0 ... A3 (gpio0 pins 3,4,28,29) */
+#define LED1_PIN 13
+const uint8_t buttons_pins[] = {11,12,24,25,3,4,28,29}; /* vector with pins where buttons are connected */
+	
+/* Get node ID for GPIO0, which has leds and buttons */ 
+#define GPIO0_NODE DT_NODELABEL(gpio0)
+
+/* Now get the device pointer for GPIO0 */
+static const struct device * gpio0_dev = DEVICE_DT_GET(GPIO0_NODE);
+
+/* Define a variable of type static struct gpio_callback, which will latter be used to install the callback
+*  It defines e.g. which pin triggers the callback and the address of the function */
+static struct gpio_callback button_cb_data;
+
+/* Define a callback function. It is like an ISR (and runs in the cotext of an ISR) */
+/* that is called when the button is pressed */
+volatile int up,down,select,return_credit,button_touch=0;
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	int i=0;
+	button_touch=1;
+    /* Toggle led1 */
+	gpio_pin_toggle(gpio0_dev,LED1_PIN);
+
+	/* Identify the button(s) that was(ere) hit*/
+	for(i=0; i<sizeof(buttons_pins); i++){		
+		if(BIT(buttons_pins[i]) & pins) {
+			switch(i+1){
+				case 1: /* up */
+					up=-1;
+					printk("up \n");
+					break;
+				case 2:/* select */
+					select=1;
+					printk("select\n");
+					break;
+				case 3:/* down */
+					down=1;
+					printk("down\n");
+					break;
+				case 4:/* return_credit */
+					return_credit=1;
+					printk("return_credit\n");
+					break;
+				default:
+					break;
+			}
+		}
+	} 
+}
+
+void update_menu(int a,char b[][20],int c){
+	int i;
+	for (i = 0; i <= c; ++i)
+	{
+		if(i==a){
+			printf("-->");
+		}
+		else{
+			printf("   ");
+		}
+		printf("%s\n",b[i]);
+	}
+	/*printk("\n\n\n\n\n\n\n\n\n");*/
+}
+void reset_buttons(void){
+	up=0;
+	down=0;
+	select=0;
+	return_credit=0;
+	button_touch=0;
+}
+/* 
+ * The main function
+ */
+void main(void)
+{
+	int ret, i;
+	uint32_t pinmask = 0; /* Mask for setting the pins that shall generate interrupts */
+	
+	/* Welcome message */
+	printk("Digital IO accessing IO pins not set via DT (external buttons in the case) \n\r");
+	printk("Hit buttons 1-8 (1...4 internal, 5-8 external connected to A0...A3). Led toggles and button ID printed at console \n\r");
+
+	/* Check if gpio0 device is ready */
+	if (!device_is_ready(gpio0_dev)) {
+		printk("Error: gpio0 device is not ready\n");
+		return;
+	} else {
+		printk("Success: gpio0 device is ready\n");
+	}
+
+	/* Configure the GPIO pins - LED1 for output and buttons 1-4 + IOPINS 2,4,28 and 29 for input
+	 * Use internal pull-up to avoid the need for an external resistor (buttons) */
+	ret = gpio_pin_configure(gpio0_dev,LED1_PIN, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		printk("Error: gpio_pin_configure failed for led1, error:%d\n\r", ret);
+		return;
+	}
+
+	for(i=0; i<sizeof(buttons_pins); i++) {
+		ret = gpio_pin_configure(gpio0_dev, buttons_pins[i], GPIO_INPUT | GPIO_PULL_UP);
+		if (ret < 0) {
+			printk("Error: gpio_pin_configure failed for button %d/pin %d, error:%d\n\r", i+1,buttons_pins[i], ret);
+			return;
+		} else {
+			printk("Success: gpio_pin_configure for button %d/pin %d\n\r", i+1,buttons_pins[i]);
+		}
+	}
+
+	/* Configure the interrupt on the button's pin */
+	for(i=0; i<sizeof(buttons_pins); i++) {
+		ret = gpio_pin_interrupt_configure(gpio0_dev, buttons_pins[i], GPIO_INT_EDGE_TO_ACTIVE );
+		if (ret < 0) {
+			printk("Error: gpio_pin_interrupt_configure failed for button %d / pin %d, error:%d", i+1, buttons_pins[i], ret);
+			return;
+		}
+	}
+
+	/* HW init done!*/
+	printk("All devices initialized sucesfully!\n\r");
+
+	/* Initialize the static struct gpio_callback variable   */
+	pinmask=0;
+	for(i=0; i<sizeof(buttons_pins); i++) {
+		pinmask |= BIT(buttons_pins[i]);
+	}
+    gpio_init_callback(&button_cb_data, button_pressed, pinmask); 	
+	
+	/* Add the callback function by calling gpio_add_callback()   */
+	gpio_add_callback(gpio0_dev, &button_cb_data);
+
+	/* 
+ 	 * The main loop
+ 	 */ 
+	typedef enum{
+		NULL_CREDIT = 0,
+		CREDIT,
+		MOVIE,
+		SESSION,
+		TICKET
+	}state_t;
+	int pointer=0;
+	state_t current_state=NULL_CREDIT;
+	state_t next_state=MOVIE;
+	char menu_NULL_CREDIT[20]={"Insert credit:"};
+	char menu_MOVIE[3][20]={"Movie A","Movie B"};
+	char menu_SESSION[1][20]={"temp"};
+	(void)update_menu(pointer,menu_MOVIE,2);
+	while (1) {
+		switch(next_state){
+			case NULL_CREDIT:
+				break;
+			case CREDIT:
+				printk("CREDITSZ");
+				break;
+			case MOVIE:
+				if(button_touch==1){
+					if(up==-1 || down==1){
+						pointer=(pointer+up+down)%2;
+						printk("Movies\n");
+						(void)update_menu(pointer,menu_MOVIE,2);
+						(void)reset_buttons();
+					}
+					else if(select==1){
+						if(pointer==0){
+							current_state=next_state;
+							next_state=SESSION;
+							(void)update_menu(pointer,menu_SESSION,1);
+							(void)reset_buttons();
+						}
+						else if(pointer==1){
+							pointer=0;
+							current_state=next_state;
+							next_state=SESSION;
+							(void)update_menu(pointer,menu_SESSION,1);
+							(void)reset_buttons();
+						}
+						else if(return_credit==1){
+							pointer=0;
+							current_state=next_state;
+							next_state=CREDIT;
+							(void)reset_buttons();
+						}
+						else{
+							printf("ERROR ENTERING NEXT STATE DUE TO POINTER VALUE\n");
+						}
+					}
+				}
+				break;
+			case SESSION:
+				if(button_touch==1){
+					printk("Hello!\n");
+					(void)reset_buttons();
+				}
+				break;
+			case TICKET:
+				break;
+			default:
+				break;
+		}
+		/* Just sleep. Led on/off is done by the int callback */
+		k_msleep(SLEEP_TIME_MS);
+	}
+}
